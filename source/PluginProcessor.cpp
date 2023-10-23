d8b284a619224c11a8be98f4276334586dcbc454
jguarin <jguarin@codebranch.co>
2023-10-23 16:04:26 -0500

    Get block into a stl container

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -147,21 +147,26 @@ void AudioStreamPluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     }
 
 
-
-
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
+    auto totalNumberOfSamples = buffer.getNumSamples();
 
-    // In case we have more outputs than inputs, this code clears any output
+    std::vector<std::vector<float>> outStreamDataChannels (totalNumOutputChannels * totalNumberOfSamples);
+    std::vector<std::vector<float>> inStreamDataChannels (totalNumInputChannels * totalNumberOfSamples);
+
+    // In case we have more outputs than inputßs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
     // This is here to avoid people getting screaming feedback
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+    {
         buffer.clear (i, 0, buffer.getNumSamples());
-
+        auto& outStreamDataChannel = outStreamDataChannels[i];
+        outStreamDataChannel.clear();
+    }
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
     // Make sure to reset the state if your inner loop is processing
@@ -171,8 +176,11 @@ void AudioStreamPluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+        for (int sample = 0; sample < totalNumberOfSamples; ++sample)
+        {
+            channelData[sample] = *(channelData + sample);
+
+        }
     }
 }
 
